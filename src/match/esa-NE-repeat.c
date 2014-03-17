61faf7e39bab23605e13129f1543f6fe49d6c979
Philipp Lutz Carpus <random234@gmx.net>
2014-03-17 19:09:12 +0100

    added esa-NE-repeat.c & h to search for non-extendible repeats

---
+++ src/match/esa-NE-repeat.c
@@ -0,0 +1,88 @@
+/*
+  Copyright (c) 2014 Philipp Lutz Carpus <random234@zbh.uni-hamburg.de>
+  Copyright (c) 2014 Center for Bioinformatics, University of Hamburg
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#include "esa-seqread.h"
+#include "core/encseq_api.h"
+#include "core/showtime.h"
+
+/* vorraussichtlice Stackgroesse muss geschaetzt werden um 
+ * konstante Stackgroesse sinnvoll zu waehlen.
+ */
+
+int gt_run_NE_repeats(GtStrArray *inputindex,
+    GtUword searchlength,
+    bool absolute,
+    bool silent,
+    GT_UNUSED bool outedges,
+    GtLogger *logger,
+    GtError *err)
+{
+  bool haserr = false;
+  gt_error_check(err);
+  Sequentialsuffixarrayreader *ssar =
+    gt_newSequentialsuffixarrayreaderfromfile(gt_str_array_get(
+        inputindex,0),
+        SARR_LCPTAB |
+        SARR_SUFTAB |
+        SARR_ESQTAB,
+        false,
+        /* SEQ_scan, */
+        logger,
+        err);
+  GtTimer *nerepeat_progress = NULL;
+  char method = 'D';
+  GT_UNUSED GtUword lcpvalue,
+            previoussuffix,
+            idx,
+            nonspecials,
+            sumsuftab,
+            sumlcptab,
+            max;
+
+  GT_UNUSED const GtEncseq *encseq = gt_encseqSequentialsuffixarrayreader(ssar);
+  nonspecials = gt_Sequentialsuffixarrayreader_nonspecials(ssar);
+
+  gt_showtime_enable();
+  if (gt_showtime_enabled())
+  {
+    nerepeat_progress = gt_timer_new_with_progress_description(
+        "finding non-extendible repeats");
+  gt_timer_start(nerepeat_progress);
+  }
+
+  for (idx = 0; idx < nonspecials; idx++)
+  {
+    SSAR_NEXTSEQUENTIALLCPTABVALUE(lcpvalue,ssar);
+    sumlcptab += lcpvalue; /* silly but guarantees that loop is not
+                              eliminated by compiler */
+    SSAR_NEXTSEQUENTIALSUFTABVALUE(previoussuffix,ssar);
+    sumsuftab += previoussuffix; /* silly but guarantees that loop is not
+                                  eliminated by compiler */
+  }
+
+  if (nerepeat_progress != NULL)
+  {
+    gt_timer_show_progress_final(nerepeat_progress, stdout);
+    gt_timer_delete(nerepeat_progress);
+  }
+
+  if (ssar != NULL)
+  {
+    gt_freeSequentialsuffixarrayreader(&ssar);
+  }
+  return haserr ? -1 : 0;
+}
