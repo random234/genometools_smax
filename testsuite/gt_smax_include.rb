ee185f7b86676f24f56f7b7893c7225f5fb94bd5
Philipp Lutz Carpus <random234@gmx.net>
2014-03-24 19:54:35 +0100

    improved suffix generation in testcases by dropping unneeded tables

--- testsuite/gt_smax_include.rb
+++ testsuite/gt_smax_include.rb
@@ -1,24 +1,24 @@
 Name "gt smax map only index"
 Keywords "gt_smax"
 Test do 
-  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds" +
-    " -lcp -tis -des -ssp"   
+  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds no" +
+    " -lcp -des no -md5 no"   
   run_test "#{$bin}gt smax -ii Random.fna -map"
 end
 
 Name "gt smax map absolute"
 Keywords "gt_smax"
 Test do 
-  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds" +
-    " -lcp -tis -des -ssp"   
+  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds no" +
+    " -lcp -des no -md5 no"   
   run_test "#{$bin}gt smax -ii Random.fna -absolute -map"
 end
 
 Name "gt smax map diff absolute"
 Keywords "gt_smax"
 Test do
-  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds" +
-    " -lcp -tis -des -ssp"
+  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds no" +
+    " -lcp -des no -md5 no"
   run_test "#{$bin}gt smax -ii Random.fna -absolute -l 2 -map"
   run_test "grep -v '# argv' #{last_stdout}"
   run_test "diff #{last_stdout} #{$testdata}Random.fna_smax_absolute.out"
@@ -27,8 +27,8 @@ end
 Name "gt smax map diff relative"
 Keywords "gt_smax"
 Test do
-  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds" +
-    " -lcp -tis -des -ssp"
+  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds no" +
+    " -lcp -des no -md5 no"
   run_test "#{$bin}gt smax -ii Random.fna -l 2 -map"
   run_test "grep -v '# argv' #{last_stdout}"
   run_test "diff #{last_stdout} #{$testdata}Random.fna_smax_relative.out"
@@ -37,35 +37,35 @@ end
 Name "gt smax scan only index"
 Keywords "gt_smax"
 Test do
-    run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds" +
-          " -lcp -tis -des -ssp"   
-    run_test "#{$bin}gt smax -ii Random.fna"
+  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds no" +
+    " -lcp -des no -md5 no"   
+  run_test "#{$bin}gt smax -ii Random.fna"
 end
   
 Name "gt smax scan absolute"
 Keywords "gt_smax"
 Test do
-    run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds" +
-          " -lcp -tis -des -ssp" 
-    run_test "#{$bin}gt smax -ii Random.fna -absolute"
+  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds no" +
+    " -lcp -des no -md5 no" 
+  run_test "#{$bin}gt smax -ii Random.fna -absolute"
 end
 
 Name "gt smax scan diff absolute"
 Keywords "gt_smax"
 Test do
-    run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds" +
-          " -lcp -tis -des -ssp"
-    run_test "#{$bin}gt smax -ii Random.fna -absolute -l 2"
-    run_test "grep -v '# argv' #{last_stdout}"
-    run_test "diff #{last_stdout} #{$testdata}Random.fna_smax_absolute.out"
+  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds no" +
+    " -lcp -des no -md5 no"
+  run_test "#{$bin}gt smax -ii Random.fna -absolute -l 2"
+  run_test "grep -v '# argv' #{last_stdout}"
+  run_test "diff #{last_stdout} #{$testdata}Random.fna_smax_absolute.out"
 end
 
 Name "gt smax scan diff relative"
 Keywords "gt_smax"
 Test do
-    run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds" +
-          " -lcp -tis -des -ssp"
-    run_test "#{$bin}gt smax -ii Random.fna -l 2"
-    run_test "grep -v '# argv' #{last_stdout}"
-    run_test "diff #{last_stdout} #{$testdata}Random.fna_smax_relative.out"
+  run_test "#{$bin}gt suffixerator -db #{$testdata}Random.fna -dna -suf -sds no" +
+    " -lcp -des no -md5 no"
+  run_test "#{$bin}gt smax -ii Random.fna -l 2"
+  run_test "grep -v '# argv' #{last_stdout}"
+  run_test "diff #{last_stdout} #{$testdata}Random.fna_smax_relative.out"
 end
