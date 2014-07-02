c2120313791213b699aedac3d04fdabb9dbed56e
Philipp Lutz Carpus <random234@gmx.net>
2014-07-02 15:02:03 +0200

    barely functional version of esa-NE-repeat.c

--- src/match/esa-NE-repeat.c
+++ src/match/esa-NE-repeat.c
@@ -20,6 +20,7 @@
 #include "core/stack-inlined.h"
 #include "match/esa-seqread.h"
 #include "match/esa-smax.h"
+#include "core/defined-types.h"
 
 /* vorraussichtlice Stackgroesse muss geschaetzt werden um
  * konstante Stackgroesse sinnvoll zu waehlen.
@@ -29,7 +30,9 @@ typedef struct
 {
   GtUword lcp;
   GtUword lb;
-  char bwt;
+  Definedunsignedint left_context;
+  //char left_context;
+  //bool left_extendible;
 } Lcp_stackelem;
 
 GT_STACK_DECLARESTRUCT(Lcp_stackelem, 32UL);
@@ -48,6 +51,41 @@ char LEletter(char bwt1, char bwt2)
 }
 */
 
+bool check_suffix_not_null(GtUword suffix)
+{
+  if (suffix == 0) 
+  {
+    return false;
+  } else
+  {
+    return true;
+  }
+}
+
+Definedunsignedint check_suf_extendible(const GtEncseq *encseq, 
+                                          Definedunsignedint suf1, 
+                                          Definedunsignedint suf2)
+{
+  if (!suf1.defined) 
+  {
+    return suf1;
+  }
+  char sufchar1 = gt_encseq_get_decoded_char(encseq,
+                                             suf1.valueunsignedint,
+                                             GT_READMODE_FORWARD);
+  char sufchar2 = gt_encseq_get_decoded_char(encseq,
+                                              suf2.valueunsignedint,
+                                              GT_READMODE_FORWARD);
+  if (ISSPECIAL(sufchar1) || ISSPECIAL(sufchar2) || (sufchar1 != sufchar2))
+  {
+    suf1.defined = false;
+    return suf1;
+  } else 
+  {
+    return suf1;
+  }
+}
+
 int gt_run_NE_repeats(Sequentialsuffixarrayreader *ssar,
                       GT_UNUSED GtUword searchlength,
                       GT_UNUSED bool silent,
@@ -60,11 +98,15 @@ int gt_run_NE_repeats(Sequentialsuffixarrayreader *ssar,
   GtTimer *nerepeat_progress = NULL;
   GtStackLcp_stackelem lcpstack;
   Lcp_stackelem current_elem;
-  GtUword lcpvalue = 0,
+  GT_UNUSED GtUword lcpvalue = 0,
           previoussuffix = 0,
           idx,
-          nonspecials;
-//          lb;
+          nonspecials,
+          lb;
+  Definedunsignedint suf,
+          suf_prev,
+          suf_curr;
+          
 
   const GtEncseq *encseq = gt_encseqSequentialsuffixarrayreader(ssar);
   nonspecials = gt_Sequentialsuffixarrayreader_nonspecials(ssar);
@@ -80,23 +122,96 @@ int gt_run_NE_repeats(Sequentialsuffixarrayreader *ssar,
 */
 
 //  bwt1 = gt_encseq_get_encoded_char(encseq, 0, GT_READMODE_FORWARD);
-  current_elem.lcp = -1;
+
+/*  
+  current_elem.lcp = 0;
   current_elem.lb = 0;
-//  current_elem.bwt = bwt1;
-  GT_STACK_PUSH(&lcpstack,current_elem);
+  current_elem.left_context = suf_pred;
+  current_elem.left_extendible = false;
+*/
+
+//  GT_STACK_PUSH(&lcpstack,current_elem);
   for (idx = 0; idx < nonspecials; idx++)
   {
-    SSAR_NEXTSEQUENTIALLCPTABVALUE(lcpvalue,ssar);
-    SSAR_NEXTSEQUENTIALSUFTABVALUE(previoussuffix,ssar);
-    printf("lcp: " GT_WU "\t",lcpvalue);
-    printf("suf: " GT_WU "\t",previoussuffix);
-    printf("char: %c\n",  gt_encseq_get_decoded_char(encseq, previoussuffix,GT_READMODE_FORWARD));
-//    lb = idx;
-//    lb = idx;
-//    bwt2 = gt_encseq_get_decoded_char(encseq, idx+1,GT_READMODE_FORWARD);
-//    printf("bwt1: %c bwt2: %c\n", bwt1, bwt2);
-//    bwt = LEletter(bwt1, bwt2);
-//    bwt1 = bwt2;
+    if (idx == 0)
+    {
+      lb = idx;
+      SSAR_NEXTSEQUENTIALSUFTABVALUE(previoussuffix,ssar);
+      current_elem.lcp = 0;
+      current_elem.lb = lb;
+      if (check_suffix_not_null(previoussuffix))
+      {
+        suf_prev.valueunsignedint = previoussuffix-1;
+        suf_prev.defined = true;
+        current_elem.left_context = suf_prev;
+      } else
+      {
+        suf_prev.defined = false;
+        current_elem.left_context = suf_prev;
+      }
+      GT_STACK_PUSH(&lcpstack,current_elem);
+    } else
+    {
+      lb = idx;
+      SSAR_NEXTSEQUENTIALLCPTABVALUE(lcpvalue,ssar);
+      SSAR_NEXTSEQUENTIALSUFTABVALUE(previoussuffix,ssar);
+
+      if (suf_prev.defined)
+      {
+        if (check_suffix_not_null(previoussuffix))
+        {
+          suf_curr.valueunsignedint = previoussuffix-1;
+          suf_curr.defined = true;
+        }
+        suf = check_suf_extendible(encseq, suf_prev, suf_curr);
+      } else
+      {
+        suf = suf_prev;
+      }
+
+      while (GT_STACK_TOP(&lcpstack).lcp > lcpvalue)
+      {
+        current_elem = GT_STACK_POP(&lcpstack);
+        if (!current_elem.left_context.defined && lcpvalue > searchlength)
+        {
+          if (!silent)
+          {
+            printf("" GT_WU " " GT_WU " " GT_WU "\n",current_elem.lcp,
+                                                current_elem.lb, lb);
+          }
+        }
+        lb = current_elem.lb;
+        GT_STACK_TOP(&lcpstack).left_context = check_suf_extendible(
+                                      encseq,
+                                      current_elem.left_context,
+                                      GT_STACK_TOP(&lcpstack).left_context);
+        suf = check_suf_extendible(encseq, current_elem.left_context,suf);
+      }
+
+      if (GT_STACK_TOP(&lcpstack).lcp == lcpvalue)
+      {
+        GT_STACK_TOP(&lcpstack).left_context = check_suf_extendible(
+                                      encseq,
+                                      GT_STACK_TOP(&lcpstack).left_context,
+                                      suf);
+      } else
+      {
+        current_elem.lcp = lcpvalue;
+        current_elem.lb = lb;
+        current_elem.left_context = suf;
+        GT_STACK_PUSH(&lcpstack,current_elem);
+      }
+    }
+//      printf("lcp: " GT_WU "\t",lcpvalue);
+//      printf("suf: " GT_WU "\t",previoussuffix);
+//      if (check_suffix_extendible(previoussuffix))
+//      {
+//        printf("char: %c\n",  gt_encseq_get_decoded_char(encseq, previoussuffix-1,GT_READMODE_FORWARD));
+//      }
+//    }
+//    suf_pred = gt_encseq_get_decoded_char(encseq,previoussuffix-1,GT_READMODE_FORWARD);
+//    suf_curr = gt_encseq_get_decoded_char(encseq,previoussuffix, GT_READMODE_FORWARD);
+//    suf = 0; 
 /*
     while (GT_STACK_TOP(&lcpstack).lcp > lcpvalue)
     {
